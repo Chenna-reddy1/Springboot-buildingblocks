72d58398165f1b81b58fb3c86254a28cc78987f0
Chenna-reddy1 <mvcreddy.898@gmail.com>
2020-11-27 16:40:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringbootBuildingblocks1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocks1Application.class, args);
+	}
+
+}
